d582492bc163faec210c801e827a020214df4a62
drampas <drampasaggelos@gmail.com>
2023-06-13 14:46:15 +0300

    test fix

--- src/test/java/drampas/springframework/recipeapp/controllers/RecipeShowControllerTest.java
+++ src/test/java/drampas/springframework/recipeapp/controllers/RecipeShowControllerTest.java
@@ -71,9 +71,10 @@ class RecipeShowControllerTest {
         when(recipeService.saveRecipeCommand(any())).thenReturn(command);
 
         mockMvc.perform(post("/recipe/")
-                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-//                .param("id", "")
-//                .param("description", "some string")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+                .param("description", "some string")
+                .param("directions","some directions")
                 )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/show/2"));
